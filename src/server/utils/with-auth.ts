259d9da91520c38ab2349d847569403b94065503
leendrew <leendrew99@gmail.com>
2024-02-23 00:55:43 +0600

    server/feat: request auth middleware

---
+++ src/server/utils/with-auth.ts
@@ -0,0 +1,27 @@
+import { jwtService } from '@/server/services';
+import { prisma } from '@/server/prisma';
+import { unauthorizedException } from '@/server/exceptions';
+
+export function withAuth() {
+  return defineRequestMiddleware(async (event) => {
+    const { jwtSecret } = useRuntimeConfig();
+
+    const authHeader = getHeader(event, 'Authorization');
+    if (!authHeader) {
+      throw createError({ statusCode: 401 });
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    const { sub: id } = await jwtService.verifyAsync<{ sub: number }>(token as string, {
+      secret: jwtSecret,
+    });
+
+    const user = await prisma.user.findFirst({ where: { id } });
+    if (!user) {
+      throw unauthorizedException();
+    }
+
+    event.context.user = user;
+  });
+}
