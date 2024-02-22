af48e947e3a894126b3e13be2888a49298378c00
leendrew <leendrew99@gmail.com>
2024-02-23 00:32:02 +0600

    client/chore: base css; client/chore: base page meta

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+import '@/assets/styles/main.css';
+
 useHead({
+  htmlAttrs: {
+    lang: 'ru',
+  },
+  meta: [
+    {
+      charset: 'utf-8',
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1.0',
+    },
+  ],
   title: 'Clone VK',
 });
 </script>
 
 <template>
-  <NuxtLoadingIndicator :duration="50000" />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
