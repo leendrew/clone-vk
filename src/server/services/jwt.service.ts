db73bcc6bd77fbbce5b021013160b7537ecf9e37
leendrew <leendrew99@gmail.com>
2024-02-23 01:06:47 +0600

    server/chore: little refactor jwt service

--- src/server/services/jwt.service.ts
+++ src/server/services/jwt.service.ts
@@ -1,13 +1,13 @@
 import jwt from 'jsonwebtoken';
 
+type Secret = { secret: jwt.Secret };
+
 export const jwtService = {
   signAsync(
     payload: string | Buffer | object,
-    options: jwt.SignOptions & { secret: jwt.Secret },
+    { secret, ...rest }: jwt.SignOptions & Secret,
   ): Promise<string> {
     return new Promise((resolve, reject) => {
-      const { secret, ...rest } = options;
-
       jwt.sign(payload, secret, rest, (err, result) => {
         if (err) {
           reject(err);
@@ -19,11 +19,9 @@ export const jwtService = {
   },
   verifyAsync<T extends object>(
     token: string,
-    options: jwt.VerifyOptions & { secret: jwt.Secret },
+    { secret, ...rest }: jwt.VerifyOptions & Secret,
   ): Promise<T> {
     return new Promise((resolve, reject) => {
-      const { secret, ...rest } = options;
-
       jwt.verify(token, secret, rest, (err, result) => {
         if (err) {
           reject(err);
